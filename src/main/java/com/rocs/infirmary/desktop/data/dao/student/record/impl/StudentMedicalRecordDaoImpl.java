21ad65c256a2e6b3c2a3d7963b92a68af1e847aa
Noaflores <noahelijahflores07@gmail.com>
2025-04-06 21:15:57 +0800

    IA-5 Create Student Medical Records

---
+++ src/main/java/com/rocs/infirmary/desktop/data/dao/student/record/impl/StudentMedicalRecordDaoImpl.java
@@ -0,0 +1,195 @@
+package com.rocs.infirmary.desktop.data.dao.student.record.impl;
+
+import com.rocs.infirmary.desktop.data.connection.ConnectionHelper;
+import com.rocs.infirmary.desktop.data.dao.utils.queryconstants.student.QueryConstants;
+import com.rocs.infirmary.desktop.data.model.person.student.MedicalRecord;
+import com.rocs.infirmary.desktop.data.model.person.student.Student;
+import com.rocs.infirmary.desktop.data.dao.student.record.StudentMedicalRecordDao;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The StudentMedicalRecordDaoImpl class implements the StudentMedicalRecordDao interface
+ * it provides methods for interacting with the infirmary database.
+ * It includes methods for retrieving, adding, updating, and deleting student medical records.
+ */
+public class StudentMedicalRecordDaoImpl implements StudentMedicalRecordDao {
+
+
+    public Student getMedicalInformationByLRN(long LRN) {
+
+        Student studentMedicalRecord = null;
+        try (Connection con = ConnectionHelper.getConnection()) {
+
+            QueryConstants queryConstants = new QueryConstants();
+
+            String sql = queryConstants.getAllMedicalInformationByLRN();
+
+            PreparedStatement stmt = con.prepareStatement(sql);
+
+
+            stmt.setLong(1, LRN);
+            ResultSet rs = stmt.executeQuery();
+
+
+            if (rs.next()) {
+                studentMedicalRecord = new Student();
+                studentMedicalRecord.setStudentId(rs.getInt("student_id"));
+                studentMedicalRecord.setLrn(rs.getLong("LRN"));
+                studentMedicalRecord.setFirstName(rs.getString("first_name"));
+                studentMedicalRecord.setMiddleName(rs.getString("middle_name"));
+                studentMedicalRecord.setLastName(rs.getString("last_name"));
+                studentMedicalRecord.setAge(rs.getInt("age"));
+                studentMedicalRecord.setGender(rs.getString("gender"));
+                studentMedicalRecord.setSymptoms(rs.getString("symptoms"));
+                studentMedicalRecord.setTemperatureReadings(rs.getString("temperature_readings"));
+                studentMedicalRecord.setVisitDate(rs.getDate("visit_date"));
+                studentMedicalRecord.setTreatment(rs.getString("treatment"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return studentMedicalRecord;
+
+
+    }
+
+    @Override
+    public List<Student> getAllStudentMedicalRecords() {
+        List<Student> medicalRecords = new ArrayList<>();
+        try (Connection con = ConnectionHelper.getConnection()) {
+
+            QueryConstants queryConstants = new QueryConstants();
+
+            String sql = queryConstants.getAllStudentMedicalRecords();
+
+            PreparedStatement stmt = con.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Student studentMedicalRecord = new Student();
+
+                studentMedicalRecord.setFirstName(rs.getString("first_name"));
+                studentMedicalRecord.setMiddleName(rs.getString("middle_name"));
+                studentMedicalRecord.setLastName(rs.getString("last_name"));
+                studentMedicalRecord.setAge(rs.getInt("age"));
+                studentMedicalRecord.setGender(rs.getString("gender"));
+                studentMedicalRecord.setSymptoms(rs.getString("symptoms"));
+                studentMedicalRecord.setTemperatureReadings(rs.getString("temperature_readings"));
+                studentMedicalRecord.setVisitDate(rs.getDate("visit_date"));
+                studentMedicalRecord.setTreatment(rs.getString("treatment"));
+
+                medicalRecords.add(studentMedicalRecord);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error fetching student medical records", e);
+        }
+
+        return medicalRecords;
+    }
+
+
+    /**
+     * Deactivates a student's medical record based on their LRN (Learner Reference Number).
+     * Instead of completely removing the data, it likely updates the status
+     * of the medical record in the database to indicate it's no longer active.
+     * <p>
+     * A status value of 0 means the record is no longer active (deleted),
+     * while a status of 1 means the record is still active and present in the system.
+     */
+    @Override
+    public boolean deleteStudentMedicalRecordByLrn(long LRN) {
+        Student studentMedicalRecord = getStudent(LRN);
+
+        try (Connection con = ConnectionHelper.getConnection()) {
+
+            QueryConstants queryConstants = new QueryConstants();
+
+            String sql = queryConstants.updateMedicalRecordStatus();
+
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
+            preparedStatement.setInt(1, studentMedicalRecord.getStudentId());
+
+            int affectedRow = preparedStatement.executeUpdate();
+            return affectedRow > 0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private static Student getStudent(long LRN) {
+        Student studentMedicalRecord = null;
+
+        try (Connection con = ConnectionHelper.getConnection()) {
+
+            QueryConstants queryConstants = new QueryConstants();
+
+            String sql = queryConstants.getAllMedicalInformationByLRN();
+
+            PreparedStatement stmt = con.prepareStatement(sql);
+
+            stmt.setLong(1, LRN);
+
+            ResultSet resultSet = stmt.executeQuery();
+            while (resultSet.next()) {
+                studentMedicalRecord = new Student();
+                studentMedicalRecord.setStudentId(resultSet.getInt("student_id"));
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return studentMedicalRecord;
+    }
+
+    @Override
+    public boolean createMedicalRecord(MedicalRecord medicalRecords) {
+        try (Connection con = ConnectionHelper.getConnection()) {
+            QueryConstants queryConstants = new QueryConstants();
+
+            String sql = queryConstants.getInsertMedicalRecord();
+
+            try (PreparedStatement stmt = con.prepareStatement(sql)) {
+                stmt.setLong(1, medicalRecords.getStudentId());
+                stmt.setLong(2, medicalRecords.getAilmentId());
+                stmt.setNull(3, Types.INTEGER);
+                stmt.setLong(4, medicalRecords.getNurseInChargeId());
+                stmt.setString(5, medicalRecords.getSymptoms());
+                stmt.setString(6, medicalRecords.getTemperatureReadings());
+
+                if (medicalRecords.getVisitDate() != null) {
+                    stmt.setTimestamp(7, medicalRecords.getVisitDate());
+                } else {
+                    stmt.setNull(7, Types.TIMESTAMP);
+                }
+
+                stmt.setString(8, medicalRecords.getTreatment());
+                stmt.setInt(9, 1);
+
+                int rowsAffected = stmt.executeUpdate();
+                return rowsAffected > 0;
+            } catch (SQLException e) {
+                System.err.println("SQL Error in createMedicalRecord: " + e.getMessage());
+                return false;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to the database", e);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/rocs/infirmary/desktop/data/dao/utils/queryconstants/student/QueryConstants.java
@@ -0,0 +1,51 @@
+package com.rocs.infirmary.desktop.data.dao.utils.queryconstants.student;
+
+public class QueryConstants {
+
+    private final String GET_ALL_MEDICAL_INFORMATION_BY_LRN = "SELECT " +
+            "s.id AS student_id, " +
+            "s.LRN, " +
+            "p.first_name, " +
+            "p.middle_name, " +
+            "p.last_name, " +
+            "p.age, " +
+            "p.gender, " +
+            "mr.symptoms, " +
+            "mr.temperature_readings, " +
+            "mr.visit_date AS visit_date, " +
+            "mr.treatment " +
+            "FROM student s " +
+            "JOIN person p ON s.person_id = p.id " +
+            "LEFT JOIN medical_record mr ON s.id = mr.student_id " +
+            "WHERE s.LRN = ?";
+
+    private final String GET_ALL_STUDENTS_MEDICAL_RECORDS = "SELECT " +
+            "student.id, " +
+            "person.first_name, " +
+            "person.middle_name, " +
+            "person.last_name, " +
+            "person.age, " +
+            "person.gender, " +
+            "medical_record.symptoms, " +
+            "medical_record.temperature_readings, " +
+            "medical_record.visit_date, " +
+            "medical_record.treatment " +
+            "FROM medical_record " +
+            "JOIN person ON medical_record.student_id = person.id " +
+            "LEFT JOIN student ON medical_record.student_id = student.id";
+
+
+    private final String UPDATE_STUDENT_MEDICAL_RECORD_STATUS = "UPDATE MEDICAL_RECORD SET STATUS = 0 WHERE STUDENT_ID = ?";
+
+    private final String INSERT_MEDICAL_RECORD_WITH_JOIN = "INSERT INTO medical_record (student_id, ailment_id, med_history_id, nurse_in_charge_id, symptoms, temperature_readings, visit_date, treatment, status) " +
+            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+
+    public String getAllStudentMedicalRecords() { return GET_ALL_STUDENTS_MEDICAL_RECORDS;
+    }
+    public String getAllMedicalInformationByLRN() { return GET_ALL_MEDICAL_INFORMATION_BY_LRN;
+    }
+    public String updateMedicalRecordStatus() { return UPDATE_STUDENT_MEDICAL_RECORD_STATUS;
+    }
+    public String getInsertMedicalRecord() { return INSERT_MEDICAL_RECORD_WITH_JOIN;}
+}

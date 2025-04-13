45fd385973feeb9212efed70275c3334af049e63
Noaflores <noahelijahflores07@gmail.com>
2025-04-13 21:32:39 +0800

    Placeholder

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.rocs.java.practice.application.javafxpractice {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.rocs.java.practice.application.javafxpractice to javafx.fxml;
-    exports com.rocs.java.practice.application.javafxpractice;
-}

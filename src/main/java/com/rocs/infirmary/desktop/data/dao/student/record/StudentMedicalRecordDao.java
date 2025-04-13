1ed030dd817ead669a9e8575f0145f8b41546f56
Noaflores <noahelijahflores07@gmail.com>
2025-04-13 22:08:39 +0800

    Placeholder

---
+++ src/main/java/com/rocs/infirmary/desktop/data/dao/student/record/StudentMedicalRecordDao.java
@@ -0,0 +1,25 @@
+package com.rocs.infirmary.desktop.data.dao.student.record;
+
+
+import com.rocs.infirmary.desktop.data.model.person.student.Student;
+
+import java.util.List;
+
+public interface StudentMedicalRecordDao {
+
+    Student getMedicalInformationByLRN(long LRN);
+
+    List<Student> getAllStudentMedicalRecords();
+
+
+    /**
+     * This intended to delete a student's medical record based on their Learner Reference Number (LRN).
+     * The LRN is a unique identifier assigned to each student. This value is used to locate and delete the corresponding medical record.
+     *
+     */
+
+    boolean deleteStudentMedicalRecordByLrn (long LRN);
+
+}
+
+
